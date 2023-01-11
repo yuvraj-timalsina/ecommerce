52cada1637121f71a2d9d956448cd7263efa6f09
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-01-11 09:16:02 +0545

    Cart Quantity

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,23 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class CartComponent extends Component
 {
+    public function increaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $quantity = $product->qty + 1;
+        Cart::update($rowId, $quantity);
+    }
+
+     public function decreaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $quantity = $product->qty - 1;
+        Cart::update($rowId, $quantity);
+    }
     public function render()
     {
         return view('livewire.cart-component');
