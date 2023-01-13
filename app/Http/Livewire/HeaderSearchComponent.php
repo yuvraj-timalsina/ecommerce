ee74d771fc72f7cc5331e186885b098e678ba418
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-01-13 15:22:27 +0545

    Search fix

--- app/Http/Livewire/HeaderSearchComponent.php
+++ app/Http/Livewire/HeaderSearchComponent.php
@@ -3,10 +3,20 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Contracts\View\View;
 
 class HeaderSearchComponent extends Component
 {
-    public function render()
+    public $query;
+
+
+    public function mount(): void
+    {
+        $this->fill(request()?->only('query'));
+    }
+
+
+    public function render(): View
     {
         return view('livewire.header-search-component');
     }
--- resources/views/livewire/header-search-component.blade.php
+++ resources/views/livewire/header-search-component.blade.php
@@ -1,5 +1,5 @@
 <div class="search-style-1">
 	<form action="{{route('product.search')}}">
-		<input name="query" type="text" placeholder="Search for Products...">
+		<input name="query" type="text" placeholder="Search for Products..." value="{{$query}}">
 	</form>
 </div>
